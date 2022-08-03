1a28cb50bbbdd1e044732625c8d9d9e205a2f72b
Juan Manuel Parrilla Madrid <jparrill@redhat.com>
2022-08-03 09:26:52 +0200

    forgot to save entity doc
    Signed-off-by: Juan Manuel Parrilla Madrid <jparrill@redhat.com>

--- internal/entities/history.entity.go
+++ internal/entities/history.entity.go
@@ -17,14 +17,17 @@ type History struct {
 	Success        bool `db:"success" json:"-"`
 }
 
+// TableName function returns the TableName
 func (h *History) TableName() string {
 	return "history"
 }
 
+// ToJson returns string formatted history struct
 func (h History) ToString() string {
 	return fmt.Sprintf("id: %d\norg_id: %d\nmlmodel_id: %d\nsuccess: %v", h.ID, h.OrganizationID, h.MLModelID, h.Success)
 }
 
+// ToJson returns JSON formatted history struct
 func (h History) ToJson() ([]byte, error) {
 	return json.Marshal(&h)
 }
